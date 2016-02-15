55d8257a8e779c8ed2d37b592cc24541989b3409
heroico <soyunheroe@gmail.com>
2016-02-15 09:00:53 -0300

    Missing files

---
+++ software/DataSetSNP.py
@@ -0,0 +1,11 @@
+__author__ = 'heroico'
+
+import DataSet
+
+class DataSetSNP(DataSet.DataSet):
+    """" A dat aset form an snp """
+    def __init__(self, name=None, index=None, data=[], position=None, ref_allele=None, eff_allele=None):
+        super(DataSetSNP, self).__init__(name, index, data)
+        self.position = position
+        self.ref_allele = ref_allele
+        self.eff_allele = eff_allele

95abd7383cd20cefcb4d8beee42a96eb1aee76f9
Alvaro <alvarobarbeira@gmail.com>
2016-09-13 14:53:28 -0500

    Started working towards test, squash this

---
+++ software/tests/_td/beta_builder.R
@@ -0,0 +1,6 @@
+#! /usr/bin/env Rscript
+
+#This script takes some data from a DIAGRAM GWAS file and  \
+"rs1937888"
+"rs17155745"
+"rs62626328"
---
+++ software/tests/test_M03_betas.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import unittest
+import sys
+import shutil
+import os
+import re
+import gzip
+
+if "DEBUG" in sys.argv:
+    sys.path.insert(0, "..")
+    sys.path.insert(0, "../../")
+    sys.path.insert(0, ".")
+    sys.argv.remove("DEBUG")
+
+import M03_betas
+
+class TestM03(unittest.TestCase):
+    def testNoWeightDBMode(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == "__main__":
+    unittest.main()

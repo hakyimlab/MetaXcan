1a7f5f284913f1cb50189c6c78abbc0ed96e623c
Fnyasimi <41294948+Fnyasimi@users.noreply.github.com>
2024-10-08 12:27:21 -0500

    highlight the warning in red for easier visibility of missing params

--- software/metax/Logging.py
+++ software/metax/Logging.py
@@ -13,9 +13,20 @@ def configureLogging(level=5, target=sys.stderr):
         logger.handlers.clear()
 
     # create console handler and set level to info
+    RED = "\033[91m"
+    RESET = "\033[0m"
+
+    # A custom logging formatter for warning
+    class CustomFormatter(logging.Formatter):
+        def format(self, record):
+            if record.levelno == logging.WARNING:
+                record.msg = f"{RED}{record.msg}{RESET}"
+            return super().format(record)
+
+    # create console handler and set level to info
     handler = logging.StreamHandler(target)
     handler.setLevel(level)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = CustomFormatter("%(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
